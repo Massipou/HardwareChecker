5242d5721b8e1df8a5cd153410c3a9fe7c4e0cb3
Massipou <vivxxl@gmail.com>
2022-07-01 16:25:49 +0200

    Veuillez saisir le message de validation pour vos modifications. Les lignes  commençant par '' seront ignorées, et un message vide abandonne la validation.
     Sur la branche main
     Modifications qui seront validées :
    	modifié :         checkbattery.sh
    	modifié :         checkinglist
    	modifié :         darkpan/bluetooth.html
    	modifié :         darkpan/checkinglist
    	modifié :         darkpan/index.html
    	modifié :         darkpan/logs/battery.html
    	modifié :         darkpan/logs/disk.html
    	modifié :         darkpan/logs/health.js
    	modifié :         darkpan/logs/inventory.html
    	modifié :         darkpan/logs/wifi.html
    	modifié :         init.sh
    	modifié :         logs/audiologs/audio
    	modifié :         logs/battery/battery
    	modifié :         logs/bluetooth/bluedev
    	nouveau fichier : logs/disklog/*
    	modifié :         logs/fanlogs/fanlogs
    	modifié :         logs/health
    	modifié :         logs/inventory
    	modifié :         logs/wifilogs/wifiresult
    
     Modifications qui ne seront pas validées :
    	modifié :         checkinglist
    	modifié :         darkpan/logs/disk.html
    	modifié :         darkpan/logs/health.js
    	modifié :         darkpan/logs/inventory.html
    	modifié :         darkpan/logs/wifi.html
    	modifié :         islive.cfg
    	supprimé :        launcher.html
    	supprimé :        logs/audiologs/audio
    	supprimé :        logs/battery/battery
    	supprimé :        logs/bluetooth/bluedev
    	supprimé :        logs/fanlogs/fanlogs
    	supprimé :        logs/inventory
    	supprimé :        logs/wifilogs/wifiresult
    
     Fichiers non suivis:
    	live/

--- checkbattery.sh
+++ checkbattery.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "energy-full-design|energy-full|capacity|percentage" | sed 's/ *//g' | sed 's/:/=/g' > logs/battery/battery
 upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "energy-full-design|energy-full|capacity"
-capacity=$(cat logs/battery/battery|grep capacity | sed 's/capacity=//' | sed 's/%//' | sed 's/,.*//')
+capacity=$(cat logs/battery/battery|grep capacity | sed 's/capacity=//' | sed 's/%//' | sed 's/\,.*//' | sed 's/\..*//')
+echo $capacity
 if [ $capacity -ge 60 ]
 then
         echo "Capacity OK"
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 #echo -e "disk=none\nwifi=none\nbluetooth=none\naudio=none\nbattery=none" > logs/health
-sudo chown www-data:www-data darkpan/checkinglist
+#sudo chown www-data:user darkpan/checkinglist
+
 echo -e "var Heath = \n{\n    sn : \"\",\n    disk : \"\",\n    wifi : \"\",\n    bluetooth : \"\",\n    audio : \"\",\n    CPU_FAN: \"\",\n    battery : \"\"\n};" > darkpan/logs/health.js
 echo "" > logs/inventory
 echo "" > darkpan/logs/inventory.html
@@ -7,9 +9,24 @@ echo "" > darkpan/logs/disk.html
 echo "" > darkpan/logs/wifi.html
 echo "" > darkpan/logs/bluetooth.html
 echo "" > checkinglist
-if [ "$1" == "live" ]
+
+rm  logs/audiologs/*
+rm logs/battery/*
+rm logs/bluetooth/*
+rm logs/disk/disklog/*
+rm logs/fanlogs/*
+rm logs/inventory
+rm logs/wifilogs/*
+
+
+if [ "$1" = "live" ]
 then
 	echo "islive=true" > islive.cfg
+	sudo chown -R :user *
+	sudo chown -R user:user logs/
+	sudo chown -R www-data:user darkpan/
 else
 	echo "islive=false" > islive.cfg
 fi
+
+sudo chmod g+w darkpan/checkinglist
